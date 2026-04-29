48fb37f705ddb58dbf441009d7b186e41b8610bf
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 10:07:12 -0400

    added implementation

--- src/main/java/game/view/UserInterface.java
+++ src/main/java/game/view/UserInterface.java
@@ -1,8 +1,40 @@
 package game.view;
 
+import game.model.GameModel;
+import game.model.ParkingObserver;
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 /**
- * Manages game screens like start, level selection, and end screen.
+ * Main game panel that coordinates all view components and implements Observer.
  */
-public class UserInterface {
-    // Ui: start, level selection, end screen
+public class UserInterface extends JPanel implements ParkingObserver {
+    private CarView carView;
+    private ParkingLotView lotView;
+    private GameOverlay overlay;
+
+    public UserInterface() {
+        this.setPreferredSize(new Dimension(800, 600));
+        this.setFocusable(true);
+        this.carView = new CarView();
+        this.lotView = new ParkingLotView();
+        this.overlay = new GameOverlay();
+        
+        GameModel.getInstance().addObserver(this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        lotView.draw(g);
+        carView.draw(g);
+        overlay.draw(g);
+    }
+
+    @Override
+    public void update() {
+        repaint();
+    }
 }
