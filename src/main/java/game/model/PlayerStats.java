aa6671002f6c6f404d3948a1f8ab2ccb4cde00e3
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 09:54:06 -0400

    Added PlayerStats.java

---
+++ src/main/java/game/model/PlayerStats.java
@@ -0,0 +1,9 @@
+package game.model;
+
+/**
+ * Tracks player progress and statistics.
+ * Level, times, records, leaderboard place.
+ */
+public class PlayerStats {
+    // level, times, records, leaderboard place
+}
