776d6247363d911cfd441e32c58fe9ecafb5a818
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 09:52:47 -0400

    Added car.java

---
+++ src/main/java/game/model/Car.java
@@ -0,0 +1,9 @@
+package game.model;
+
+/**
+ * Represents the car's physical properties and state.
+ * Stats: speed, handling, size.
+ */
+public class Car {
+    // speed, handling, size, car state
+}
