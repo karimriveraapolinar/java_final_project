1494bebf55962d1724d06b9ccc904cd27ef970f2
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 10:06:21 -0400

    added implementation

--- src/test/java/game/model/ModelTest.java
+++ src/test/java/game/model/ModelTest.java
@@ -9,12 +9,47 @@ import static org.junit.Assert.*;
 public class ModelTest {
 
     @Test
-    public void testCarStats() {
-        // Placeholder for car stats verification
+    public void testSingleton() {
+        GameModel instance1 = GameModel.getInstance();
+        GameModel instance2 = GameModel.getInstance();
+        assertSame("GameModel should be a singleton", instance1, instance2);
+    }
+
+    @Test
+    public void testCarMovement() {
+        Car car = new Car(100, 100);
+        car.accelerate();
+        car.move();
+        assertTrue("Car should have moved", car.getX() > 100 || car.getY() > 100);
     }
 
     @Test
     public void testPlayerStats() {
-        // Placeholder for player records verification
+        PlayerStats stats = new PlayerStats();
+        assertEquals(1, stats.getCurrentLevel());
+        stats.setCurrentLevel(2);
+        assertEquals(2, stats.getCurrentLevel());
+    }
+
+    @Test
+    public void testGameTimer() {
+        GameTimer timer = new GameTimer();
+        timer.start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
+        timer.stop();
+        assertTrue("Timer should have recorded time", timer.getElapsedTime() >= 100);
+    }
+
+    @Test
+    public void testCollisionDetection() {
+        GameModel model = GameModel.getInstance();
+        model.loadLevel(1);
+        Car car = model.getCar();
+        // Move car into an obstacle
+        car.setPosition(200, 100); 
+        model.update();
+        assertTrue("Car should be crashed", model.getCurrentStatus().isCrashed());
     }
 }
