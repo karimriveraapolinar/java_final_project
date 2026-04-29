f33dd4d070abbf508e7af15b001f96c92a3c6a6f
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 10:06:17 -0400

    added implementation

--- src/main/java/game/controller/MenuController.java
+++ src/main/java/game/controller/MenuController.java
@@ -1,8 +1,20 @@
 package game.controller;
 
+import game.model.GameModel;
+
 /**
  * Manages level selection and game browsing navigation.
  */
 public class MenuController {
-    // Selecting levels/browsing game
+    public void restartLevel() {
+        GameModel model = GameModel.getInstance();
+        model.loadLevel(model.getStats().getCurrentLevel());
+    }
+
+    public void nextLevel() {
+        GameModel model = GameModel.getInstance();
+        int next = model.getStats().getCurrentLevel() + 1;
+        if (next > 2) next = 1; // Loop back or end game
+        model.loadLevel(next);
+    }
 }
