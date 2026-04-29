e5a8aa968b232fee05df199b821c5a67bb89fdf8
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 10:06:31 -0400

    added implementation

--- src/main/java/game/model/ParkingStatus.java
+++ src/main/java/game/model/ParkingStatus.java
@@ -5,5 +5,33 @@ package game.model;
  * Success/fail, level difficulty.
  */
 public class ParkingStatus {
-    // success/fail, level difficulty
+    private boolean success;
+    private int difficulty;
+    private boolean crashed;
+
+    public ParkingStatus(int difficulty) {
+        this.difficulty = difficulty;
+        this.success = false;
+        this.crashed = false;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public boolean isCrashed() {
+        return crashed;
+    }
+
+    public void setCrashed(boolean crashed) {
+        this.crashed = crashed;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
 }
