1a188eb30be372990d17f37f7ab2e30c3a0b66d3
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 09:53:55 -0400

    Added ParkingStatus.java

---
+++ src/main/java/game/model/ParkingStatus.java
@@ -0,0 +1,9 @@
+package game.model;
+
+/**
+ * Represents the status of a parking attempt.
+ * Success/fail, level difficulty.
+ */
+public class ParkingStatus {
+    // success/fail, level difficulty
+}
