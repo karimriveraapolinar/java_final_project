632d05a3b931f390e3648a9de8dfdc91920f8212
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2026-04-29 09:53:35 -0400

    Added ModelTest.java

---
+++ src/test/java/game/model/ModelTest.java
@@ -0,0 +1,20 @@
+package game.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for the Game Model components.
+ */
+public class ModelTest {
+
+    @Test
+    public void testCarStats() {
+        // Placeholder for car stats verification
+    }
+
+    @Test
+    public void testPlayerStats() {
+        // Placeholder for player records verification
+    }
+}
